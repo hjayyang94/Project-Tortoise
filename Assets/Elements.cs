cad85ca7d2cc8ae6f16276e28fdd8dc021881ffb
Jay Yang <32497090+hjayyang94@users.noreply.github.com>
2021-03-17 23:04:56 -0700

    Setting up Visual Studio with Unity

---
+++ Assets/Elements.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elements
+{
+    public enum ElementType {
+        Fire,
+        Ice,
+        Ground,
+        Air
+    }   
+
+    public ElementType elementType;
+}
